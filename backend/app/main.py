db899ae8e00c3412a92887a1bd30d8e0d7cf71d3
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-07-19 22:24:26 +0530

    main updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://proto-mind-ai.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
