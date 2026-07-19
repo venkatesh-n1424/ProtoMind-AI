88e1f3c15d8efd40fc24b257e6a6c872aa8ca24a
Venkatesh <243717738+venkatesh-n1424@users.noreply.github.com>
2026-07-19 22:58:14 +0530

    Update api.ts

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,8 +1,8 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 export async function analyzePaper(file: File) {
   const formData = new FormData();
-
   formData.append("file", file);
 
   const response = await fetch(`${API_URL}/upload/`, {
